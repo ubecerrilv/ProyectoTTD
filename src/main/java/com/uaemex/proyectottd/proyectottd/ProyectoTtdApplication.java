6bfd108c5aa41f8b5c616749bc3469d76945e572
Ulises-Laptop_Fedora <ulisesbecerril81@gmail.com>
2024-10-14 18:47:40 -0600

    Commit inicial del proyecto

---
+++ src/main/java/com/uaemex/proyectottd/proyectottd/ProyectoTtdApplication.java
@@ -0,0 +1,13 @@
+package com.uaemex.proyectottd.proyectottd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoTtdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uaemex/proyectottd/proyectottd/ProyectoTtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uaemex.proyectottd.proyectottd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTtdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
