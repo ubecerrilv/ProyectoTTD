3e4c6312afb8c0d772cbbf9c15b983882715899b
Ulises-Laptop_Fedora <ulisesbecerril81@gmail.com>
2024-10-30 18:38:45 -0600

    Archivo de prueba HTML

--- src/main/java/com/uaemex/proyectottd/ProyectoTtdApplication.java
+++ src/main/java/com/uaemex/proyectottd/ProyectoTtdApplication.java
@@ -1,4 +1,4 @@
-package com.uaemex.proyectottd.proyectottd;
+package com.uaemex.proyectottd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/uaemex/proyectottd/ProyectoTtdApplicationTests.java
+++ src/test/java/com/uaemex/proyectottd/ProyectoTtdApplicationTests.java
@@ -1,4 +1,4 @@
-package com.uaemex.proyectottd.proyectottd;
+package com.uaemex.proyectottd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
